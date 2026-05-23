0c4ef7aa18992347ea5d042a9cc341433a843c15
T806809 <tahiyaakter94@gmail.com>
2026-05-23 22:19:05 +0600

    add layouts

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,22 +2,20 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-const MainLayout = () => {
-  return (
+   const MainLayout = () => {
+
+   return (
+
     <div className="bg-[#050816] min-h-screen text-white">
 
-      {/* Navbar */}
-      <Navbar />
-
-      {/* Page Content */}
-      <div>
+        <Navbar />
+        <div>
         <Outlet />
         <Footer />
-      </div>
+        </div>
 
-      
+</div>
 
-    </div>
   );
 };
 
