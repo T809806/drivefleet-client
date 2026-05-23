8f92b5cc6c886baad29e7e09e89961277ec262c8
T806809 <tahiyaakter94@gmail.com>
2026-05-23 22:54:14 +0600

    changes explorecars

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
-const CarDetails = () => {
-  const { id } = useParams();
-const { user } = useAuth();
-  const [car, setCar] = useState(null);
+    const CarDetails = () => {
+    const { id } = useParams();
+   const { user } = useAuth();
+   const [car, setCar] = useState(null);
 
   useEffect(() => {
+
     fetch(`http://localhost:5000/cars/${id}`)
       .then(res => res.json())
       .then(data => setCar(data));
@@ -23,7 +24,7 @@ const { user } = useAuth();
 
   const handleBooking = () => {
 
-  const bookingData = {
+    const bookingData = {
     carName: car.name,
     price: car.price,
     img: car.img,
@@ -32,12 +33,15 @@ const { user } = useAuth();
   };
 
   fetch("http://localhost:5000/bookings", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
+
+      method: "POST",
+      headers: {
+       "content-type": "application/json",
     },
+
     body: JSON.stringify(bookingData),
   })
+
     .then(res => res.json())
     .then(data => {
       alert("Booking Successful!");
@@ -45,45 +49,44 @@ const { user } = useAuth();
 };
 
   return (
-  <div className="min-h-screen bg-[#0B0F19] text-white flex items-start justify-center px-4 pt-6">
 
-      <div className="max-w-4xl mx-auto bg-[#0B0F19] rounded-2xl p-6">
+     <div className="min-h-screen bg-[#0B0F19] text-white flex items-start justify-center px-4 pt-6">
+     <div className="max-w-4xl mx-auto bg-[#0B0F19] rounded-2xl p-6">
+    <img
+     src={car.img}
+     alt={car.name}
+     className="w-full h-80 object-cover rounded-xl"
+   />
 
-        <img
-          src={car.img}
-          alt={car.name}
-          className="w-full h-80 object-cover rounded-xl"
-        />
+ <h1 className="text-3xl font-bold mt-6">  {car.name} </h1>
+ <p className="text-gray-400 mt-2">
+    {car.type} • {car.seats} Seats
+  </p>
 
-        <h1 className="text-3xl font-bold mt-6">
-          {car.name}
-        </h1>
+  <p className="text-gray-400 mt-2">
+     📍 {car.location}
+ </p>
 
-        <p className="text-gray-400 mt-2">
-          {car.type} • {car.seats} Seats
-        </p>
+  <p className="text-cyan-400 text-2xl font-bold mt-4">
+    ${car.price} / day
+  </p>
 
-        <p className="text-gray-400 mt-2">
-          📍 {car.location}
-        </p>
+  <p className="mt-5 text-gray-300">
+     {car.description}
+   </p>
 
-        <p className="text-cyan-400 text-2xl font-bold mt-4">
-          ${car.price} / day
-        </p>
-
-        <p className="mt-5 text-gray-300">
-          {car.description}
-        </p>
-
-        <button
+  <button
   onClick={handleBooking}
   className="mt-4 bg-cyan-400 text-black px-6 py-2 rounded-full"
 >
   Book Now
-</button>
-      </div>
 
-    </div>
+</button>
+
+</div>
+
+ </div>
+ 
   );
 };
 
--- src/pages/ExploreCars.jsx
+++ src/pages/ExploreCars.jsx
@@ -2,140 +2,147 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Loader from "../components/Loader";
 
-const ExploreCars = () => {
+  const ExploreCars = () => {
   const [cars, setCars] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
   const [search, setSearch] = useState("");
 
-  useEffect(() => {
-    setLoading(true);
+useEffect(() => {
 
-    fetch("http://localhost:5000/cars")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Failed to fetch cars");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setCars(data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setLoading(false);
-      });
+ setLoading(true);
+ fetch("http://localhost:5000/cars")
+
+ .then((res) => {
+   if (!res.ok) {
+   throw new Error("Failed to fetch cars");
+ }
+
+ return res.json();
+  })
+  .then((data) => {
+  setCars(data);
+  setLoading(false);
+  })
+  .catch((err) => {
+   setError(err.message);
+   setLoading(false);
+ });
   }, []);
 
  useEffect(() => {
+
   fetch(`http://localhost:5000/cars?search=${search}`)
+
     .then((res) => res.json())
     .then((data) => setCars(data));
+
 }, [search]);
 
-  // 🔵 LOADING STATE
-  if (loading) {
+ if (loading) {
+
     return <Loader />;
   }
 
-  // ❌ ERROR STATE
-  if (error) {
-    return (
-      <div className="h-screen flex flex-col justify-center items-center text-center">
-        <h2 className="text-2xl text-red-500 font-bold">
-          Something went wrong
-        </h2>
+ if (error) {
 
-        <p className="text-gray-400 mt-2">{error}</p>
+ return (
 
-        <button
-          onClick={() => window.location.reload()}
-          className="mt-5 px-5 py-2 bg-cyan-400 text-black rounded-full"
-        >
-          Retry
-        </button>
-      </div>
-    );
-  }
+ <div className="h-screen flex flex-col justify-center items-center text-center">
+ <h2 className="text-2xl text-red-500 font-bold"> Something went wrong </h2>
+ <p className="text-gray-400 mt-2"> {error} </p>
 
-  return (
-    <div className="min-h-screen bg-[#050816] text-white px-6 py-10">
+ <button
+   onClick={() => window.location.reload()}
+   className="mt-5 px-5 py-2 bg-cyan-400 text-black rounded-full"
+ >
+   Retry
 
-      {/* Header */}
-      <div className="text-center mb-8">
-        <h1 className="text-4xl font-bold">
-          Explore <span className="text-cyan-400">Cars</span>
-        </h1>
-        <p className="text-gray-400 mt-3">
-          Find your perfect car for rent
-        </p>
-      </div>
+</button>
 
-    <div className="mb-8  flex justify-center">
-  <input
-    type="text"
-    placeholder="Search cars..."
-    value={search}
-    onChange={(e) => setSearch(e.target.value)}
-    className="w-full md:w-96 px-4 py-3 rounded-xl bg-[#0B0F19] border border-white/10 text-white outline-none"
-  />
 </div>
 
-      {/* Cars Grid */}
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-7xl mx-auto">
+ );
 
-        {cars.map((car) => (
-          <div
-            key={car._id}
-            className="bg-[#0B0F19] border border-white/10 rounded-2xl p-5 
-            hover:scale-105 transition duration-300"
-          >
+ }
 
-            {/* Image */}
-            <img
-              src={car.img}
-              alt={car.name}
-              className="w-full h-40 object-cover rounded-xl mb-4"
-            />
+ return (
 
-            {/* Info */}
-            <h2 className="text-xl font-semibold">{car.name}</h2>
+    <div className="min-h-screen bg-[#050816] text-white px-6 py-10">
+   <div className="text-center mb-8">
+   <h1 className="text-4xl font-bold">
 
-            <p className="text-gray-400 mt-1">
-              {car.type} • {car.seats} Seats
-            </p>
+ Explore <span className="text-cyan-400"> Cars </span>
 
-            <p className="text-gray-400 text-sm mt-1">
-              📍 {car.location}
-            </p>
+ </h1>
+ <p className="text-gray-400 mt-3">  Find your perfect car for rent </p>
+ </div>
 
-            {/* Availability */}
-            <p
-              className={`mt-2 text-sm font-medium ${
-                car.available ? "text-green-400" : "text-red-400"
-              }`}
-            >
-              {car.available ? "Available" : "Not Available"}
-            </p>
+ <div className="mb-8  flex justify-center">
 
-            {/* Price */}
-            <p className="text-cyan-400 font-bold mt-3 text-lg">
-              ${car.price} / day
-            </p>
+<input
+  type="text"
+  placeholder="Search cars..."
+  value={search}
+  onChange={(e) => setSearch(e.target.value)}
+ className="w-full md:w-96 px-4 py-3 rounded-xl bg-[#0B0F19] border border-white/10 text-white outline-none"
 
-            {/* Button */}
-            <Link to={`/cars/${car._id}`}>
-              <button className="mt-4 w-full bg-cyan-400 hover:bg-cyan-300 text-black py-2 rounded-full font-semibold">
-                View Details
-              </button>
-            </Link>
+ />
 
-          </div>
-        ))}
+</div>
 
-      </div>
-    </div>
+ <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-7xl mx-auto">
+
+  {cars.map((car) => (
+     <div
+       key={car._id}
+       className="bg-[#0B0F19] border border-white/10 rounded-2xl p-5 
+       hover:scale-105 transition duration-300"
+
+     >
+  <img
+     src={car.img}
+     alt={car.name}
+     className="w-full h-40 object-cover rounded-xl mb-4"
+   />
+
+ <h2 className="text-xl font-semibold"> {car.name} </h2>
+ <p className="text-gray-400 mt-1">
+     {car.type} • {car.seats} Seats
+
+</p>
+
+ <p className="text-gray-400 text-sm mt-1">
+       📍 {car.location}
+ </p>
+
+  <p
+     className={`mt-2 text-sm font-medium ${
+     car.available ? "text-green-400" : "text-red-400"
+   }`}
+  >
+ {car.available ? "Available" : "Not Available"}
+  </p>
+
+ <p className="text-cyan-400 font-bold mt-3 text-lg">
+     ${car.price} / day
+ </p>
+
+ <Link to={`/cars/${car._id}`}>
+
+   <button className="mt-4 w-full bg-cyan-400 hover:bg-cyan-300 text-black py-2 rounded-full font-semibold">
+      View Details
+   </button>
+
+ </Link>
+
+ </div>
+
+ ))}
+
+ </div>
+</div>
+
   );
 };
 
